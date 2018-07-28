f17569a807c6ca69f150ab741eb960b80aaeac83
yuki <h_x04yjr0@yahoo.co.jp>
2018-07-28 04:44:43 +0000

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   get '/signup',to: 'users#new'
   
   root 'static_pages#home'
@@ -6,4 +7,7 @@ Rails.application.routes.draw do
   get '/help', to: 'static_pages#help'
   get '/about', to: 'static_pages#about'
   get '/contact', to: 'static_pages#contact'
+  post '/signup', to: 'users#create'
+  resources :users
+
 end
