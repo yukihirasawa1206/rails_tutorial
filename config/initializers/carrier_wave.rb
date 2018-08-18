c0e6ba78e5a26a56c34196cbee089004b469ef8e
yuki <h_x04yjr0@yahoo.co.jp>
2018-08-18 03:45:32 +0000

    Add Simplestorage Setting

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,10 +2,10 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       :provider => 'AWS',
-      :region => 'ENV[ap-northeast-1]',
-      :aws_access_key_id => 'AKIAJTS6SWY2NQEAW2SQ',
-      :aws_secret_access_key => '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'
+      :region => ENV['S3_REGION'],
+      :aws_access_key_id => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY'],
     }
-    config.fog_directory = 'misimplestorage'
+    config.fog_directory = ENV['S3_BUCKET']
   end
 end
